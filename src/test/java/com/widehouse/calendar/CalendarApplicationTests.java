ba7e55824138190d187e48f32cd0846d1098a6a2
kielhong <kielhong@naver.com>
2019-11-18 22:06:14 +0900

    #3 lombok, checkstyle, jacoco plugin 추가

--- src/test/java/com/widehouse/calendar/CalendarApplicationTests.java
+++ src/test/java/com/widehouse/calendar/CalendarApplicationTests.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CalendarApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }

45f3c17423281a24429719752fcda40ef192572c
kielhong <kielhong@naver.com>
2019-11-24 23:24:20 +0900

    #4 Event must have creator(User)

--- src/main/java/com/widehouse/calendar/event/Event.java
+++ src/main/java/com/widehouse/calendar/event/Event.java
@@ -3,15 +3,19 @@ package com.widehouse.calendar.event;
 import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
+import java.util.prefs.Preferences;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.widehouse.calendar.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NonNull;
+import org.springframework.util.Assert;
 
 @Getter
 @Builder
@@ -21,6 +25,8 @@ public class Event {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
+    private final User creator;
     @Column(length = 200)
     private String name;
     @Column(length = 2000)
@@ -35,11 +41,10 @@ public class Event {
          * startAt 이 endAt 보다 앞인지 검사
          */
         public Event build() {
-            if (startAt.isAfter(endAt)) {
-                throw new IllegalArgumentException("startAt is behind endAt");
-            }
+            Assert.notNull(creator, "creator should not be null");
+            Assert.isTrue(startAt.isBefore(endAt), "event start before endAt");
 
-            return new Event(id, name, description, startAt, endAt);
+            return new Event(id, creator, name, description, startAt, endAt);
         }
     }
 }
--- src/test/java/com/widehouse/calendar/event/EventTest.java
+++ src/test/java/com/widehouse/calendar/event/EventTest.java
@@ -6,9 +6,18 @@ import static org.assertj.core.api.BDDAssertions.thenThrownBy;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
+import com.widehouse.calendar.user.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class EventTest {
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = User.builder().name("user").build();
+    }
+
     @Test
     void buildTest() {
         // given
@@ -16,6 +25,7 @@ class EventTest {
         ZonedDateTime endAt = ZonedDateTime.of(2019, 11, 1, 14, 0, 0, 0, ZoneOffset.UTC);
         // when
         Event event = Event.builder()
+                        .creator(user)
                         .name("event name")
                         .description("event description")
                         .startAt(startAt)
@@ -23,10 +33,23 @@ class EventTest {
                         .build();
         // then
         then(event)
+                .hasFieldOrPropertyWithValue("creator", user)
                 .hasFieldOrPropertyWithValue("name", "event name")
                 .hasFieldOrPropertyWithValue("description", "event description")
                 .hasFieldOrPropertyWithValue("startAt", startAt)
                 .hasFieldOrPropertyWithValue("endAt", endAt);
+    }
+
+    @Test
+    void builder_NonNull_ThrowException() {
+        Event.EventBuilder eventBuilder = Event.builder()
+                .name("event name")
+                .description("event description")
+                .startAt(ZonedDateTime.of(2019, 11, 1, 13, 0, 0, 0, ZoneOffset.UTC))
+                .endAt(ZonedDateTime.of(2019, 11, 1, 15, 0, 0, 0, ZoneOffset.UTC));
+        // when
+        thenThrownBy(() -> eventBuilder.build())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
